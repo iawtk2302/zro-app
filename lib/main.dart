59f9fd77d2d193425d8729aed2dca2b438a97ce0
khoi.nguyen <khoi.nguyen@executionlab.asia>
2024-11-25 14:29:19 +0700

    distribute app

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page 1'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page 2'),
     );
   }
 }
